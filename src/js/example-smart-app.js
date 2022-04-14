40b52bf726a77a3491a86fc38cacc2c42db18ae3
billparker <bill.parker@neuralocity.com>
2022-04-14 07:53:18 -0700

    update

--- src/js/example-smart-app.js
+++ src/js/example-smart-app.js
@@ -114,21 +114,14 @@
           document.getElementById('demo-table').style.display = "block"  
           document.getElementById('main-wait').style.display = "none"  
 
-
           // PLOTLY
-          var trace1 = {
-            x: [1, 2, 3, 4],
-            y: [10, 15, 13, 17],
-            type: 'scatter'
-          };
-          
-          var trace2 = {
-            x: [1, 2, 3, 4],
-            y: [16, 5, 11, 9],
-            type: 'scatter'
-          };
-          
-          var data = [trace1, trace2];
+          var data = [
+            {
+              x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
+              y: [1, 3, 6],
+              type: 'scatter'
+            }
+          ];
           
           Plotly.newPlot('weight-chart', data);
           //--------------------------------------
