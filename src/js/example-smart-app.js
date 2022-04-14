ffda0f4b5610e362c2d48238a8465862152022f3
billparker <bill.parker@neuralocity.com>
2022-04-14 07:40:30 -0700

    update

--- src/js/example-smart-app.js
+++ src/js/example-smart-app.js
@@ -95,6 +95,8 @@
 
           console.log(p.fullname);
 
+          console.log(sodium);
+
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
           }
@@ -111,6 +113,25 @@
           document.getElementById('greeting').style.display = "block"  
           document.getElementById('demo-table').style.display = "block"  
           document.getElementById('main-wait').style.display = "none"  
+
+
+          // PLOTLY
+          var trace1 = {
+            x: [1, 2, 3, 4],
+            y: [10, 15, 13, 17],
+            type: 'scatter'
+          };
+          
+          var trace2 = {
+            x: [1, 2, 3, 4],
+            y: [16, 5, 11, 9],
+            type: 'scatter'
+          };
+          
+          var data = [trace1, trace2];
+          
+          Plotly.newPlot('weight-chart', data);
+          //--------------------------------------
 
           ret.resolve(p);
         });
