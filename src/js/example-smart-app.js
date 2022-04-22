bff1054178ae37edfcb42419898d293785a41760
billparker <bill.parker@neuralocity.com>
2022-04-22 10:53:46 -0700

    update

--- src/js/example-smart-app.js
+++ src/js/example-smart-app.js
@@ -41,6 +41,7 @@
         $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
+          var maritalStatus = patient.maritalStatus.text
 
           console.log(patient);
 
@@ -84,6 +85,7 @@
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
           p.gender = titleCase(gender);
+          p.maritalStatus = titleCase(maritalStatus)
           p.fname = titleCase(fname);
           p.lname = titleCase(lname);
           p.fullname = titleCase(fullname);
@@ -164,6 +166,7 @@
       lname: {value: ''},
       fullname: {value: ''},
       gender: {value: ''},
+      maritalStatus: {value: ''},
       birthdate: {value: ''},
       height: {value: ''},
       weight: {value: ''},
@@ -247,6 +250,7 @@
     $('#lname').html(p.lname);
     $('#fullname').html(p.fullname);
     $('#gender').html(p.gender);
+    $('#marital-status').html(p.maritalStatus);
     $('#birthdate').html(p.birthdate);
     $('#height').html(p.height);
     $('#weight').html(p.weight);
