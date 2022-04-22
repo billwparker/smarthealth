b1851b7d4cf142f5a871b6eecb04ac8ec4518272
billparker <bill.parker@neuralocity.com>
2022-04-22 10:59:22 -0700

    update

--- src/js/example-smart-app.js
+++ src/js/example-smart-app.js
@@ -41,7 +41,7 @@
         $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
-          var maritalStatus = patient.maritalStatus['text'];
+          var maritalStatus = patient.maritalStatus.text;
 
           console.log(patient);
 
