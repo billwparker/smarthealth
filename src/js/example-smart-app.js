f98ea88c89deff74b888bf184b027b3e47fa018a
billparker <bill.parker@neuralocity.com>
2022-04-26 12:55:55 -0700

    update

--- src/js/example-smart-app.js
+++ src/js/example-smart-app.js
@@ -44,29 +44,30 @@
                     'http://loinc.org|2951-2'
                   ]
             },
-            note: 'Annotation'
           }
         });
 
-        // try {
-        //   console.log('try');
-        //   var obv_c = smart.patient.api.fetchAll({
-        //     type: 'CarePlan',
-        //   });
-        //   $.when(pt, obv_c).fail(onError);
+        try {
+          console.log('try');
+          var obv_c = smart.patient.api.fetchAll({
+            type: 'CarePlan',
+            title: "title"
+          });
+          $.when(pt, obv_c).fail(onError);
 
-        //   console.log("2")
+          console.log("2")
 
-        //   $.when(pt, obv_c).done(function(patient, obv_c) {
+          $.when(pt, obv_c).done(function(patient, obv_c) {
 
-        //     console.log("B")
-        //     console.log(patient);
+            print(obv_c);
+            console.log("B")
+            console.log(patient);
 
-        //   })
-        // }
-        // catch(error) {
+          })
+        }
+        catch(error) {
 
-        // }
+        }
 
 
         $.when(pt, obv).fail(onError);
